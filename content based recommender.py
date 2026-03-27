0c8f1320502d407048acd0fe114396835d7b8619
Rishika-Jetlearn <vreakiwala.vk@gmail.com>
2026-03-27 17:47:34 +0000

    Update content based recommender.py

--- content based recommender.py
+++ content based recommender.py
@@ -1,6 +1,8 @@
 import pandas as pd
 
 movies=pd.read_csv(r"C:\Users\Manisha\Desktop\Jetlearn-python-rishika\ML-and-AI\movies_metadata.csv")
+#making it smaller
+movies=movies.iloc[:20000]
 print(movies.isna().sum())
 movies=movies[["overview","title","vote_average"]].dropna()
 print(movies.isna().sum())
@@ -17,3 +19,19 @@ from sklearn.metrics.pairwise import linear_kernel
 similarity_score=linear_kernel(vector_matrix,vector_matrix)
 print(similarity_score.shape)
 print(similarity_score[:10,:10])
+
+mapping=pd.Series(movies.index,index=movies["title"])
+print(mapping)
+
+def best_movie(title):
+    index=mapping[title]
+    scores=list(enumerate(similarity_score[index]))
+    print(scores[:10])
+    sorted_movies=sorted(scores,key=lambda x:x[1], reverse=True)
+    top_ten=sorted_movies[:10]
+    top_ten_index=[i[0]for i in top_ten]
+    print(top_ten_index)
+    final=movies["title"].iloc[top_ten_index]
+    print(final)
+
+best_movie("Toy Story")
